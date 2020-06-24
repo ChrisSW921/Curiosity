4a67d696310fe86f386bb1231474d67346a2e9e3
Chris <withscottc921@gmail.com>
2020-06-24 07:59:06 -0600

    Initial Commit

---
+++ Curiosity/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Curiosity
+//
+//  Created by Chris Withers on 6/24/20.
+//  Copyright © 2020 Chris Withers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
