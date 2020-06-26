23c345d78c145494805640b3de773c717d25f53f
Chris <withscottc921@gmail.com>
2020-06-25 21:28:58 -0600

    Added firebase and set up registering new users

--- Curiosity/Controllers/RegisterViewController.swift
+++ Curiosity/Controllers/RegisterViewController.swift
@@ -7,17 +7,36 @@
 //
 
 import UIKit
-
+import Firebase
 class RegisterViewController: UIViewController {
 
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
+    @IBOutlet weak var emailTextfield: UITextField!
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    @IBAction func registerPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    self.errorLabel.text = e.localizedDescription
+                    print(e.localizedDescription)
+                } else{
+                    //Navigate to chat view controller
+                    //self.performSegue(withIdentifier: K.registerSegue, sender: self)
+                    print("Ok")
+                }
+            }
+        }
+    }
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -25,6 +44,7 @@ class RegisterViewController: UIViewController {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
+    
+
 
 }
