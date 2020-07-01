8d53711a683d2120615f76743b054362c5e10897
Chris <withscottc921@gmail.com>
2020-07-01 10:51:42 -0600

    Finished my points feature

--- Curiosity/Controllers/MyProfileViewController.swift
+++ Curiosity/Controllers/MyProfileViewController.swift
@@ -12,50 +12,62 @@ import Firebase
 class MyProfileViewController: UIViewController {
 
 
-
+    @IBOutlet weak var points: UILabel!
+    
     @IBOutlet weak var myAnswers: UILabel!
 
+    var questions: [Question] = []
 
     let db = Firestore.firestore()
 
-    //var myQuestionsAnswered: [Answer] = []
+    var myQuestionsAnswered: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //findQuestionsAnswered()
+        loadQuestions()
 
 
     }
-//
-//    func findQuestionsAnswered () {
-//            db.collection("Answers")
-//                .addSnapshotListener { (querySnapshot, error) in
-//                    if let e = error {
-//                        print("There was an issue retrieving data from firestore. \(e)")
-//                    }else {
-//                        if let snapshotDocuments = querySnapshot?.documents {
-//                            for doc in snapshotDocuments {
-//                                let data = doc.data()
-//                                if let answerText = data["Answer"] as? String, let question = data["Question"] as? String, let user = data["User"] as? String, let correctAnswer = data["Correct Answer"] as? Bool {
-//                                    var newAnswer = Answer()
-//                                    newAnswer.question = question
-//                                    newAnswer.correctAnswer = correctAnswer
-//                                    newAnswer.answer = "A: \(answerText)"
-//                                    newAnswer.user = user
-//                                    if user == Auth.auth().currentUser?.email {
-//                                        self.myQuestionsAnswered.append(newAnswer)
-//
-//                                    }
-//
-//                                }
-//                            }
-//                        }
-//                        //self.questionsAnswered.text = String(self.myQuestionsAnswered.count)
-//                        self.myAnswers.text = "My Questions Answered: \(String(self.myQuestionsAnswered.count))"
-//                    }
-//            }
-//
-//    }
+    
+func loadQuestions() {
+    db.collection("Questions")
+            .addSnapshotListener { (querySnapshot, error) in
+            self.questions = []
+            if let e = error {
+                print("There was an issue retrieving data from firestore. \(e)")
+            }else {
+                if let snapshotDocuments = querySnapshot?.documents {
+                    for doc in snapshotDocuments {
+                        let data = doc.data()
+                        if let questionText = data["Question"] as? String, let categoryOfQ = data["Category"] as? String, let user = data["User"] as? String, let QsAnswers = data["Answers"] as? [String], let UsersWhoAnswered = data["usersWhoAnswered"] as? [String], let correctA = data["CorrectAnswer"] as? [String] {
+                            var newQuestion = Question()
+                            newQuestion.category = categoryOfQ
+                            newQuestion.question = questionText
+                            newQuestion.user = user
+                            newQuestion.answers = QsAnswers
+                            newQuestion.correctAnswer = correctA
+                            newQuestion.usersWhoAnswered = UsersWhoAnswered
+                            if newQuestion.usersWhoAnswered.contains((Auth.auth().currentUser?.email)!) {
+                                self.questions.append(newQuestion)
+                            }
+                            
+                        }
+                    
+                    }
+                    self.myAnswers.text = "My questions Answered: \(String(self.questions.count))"
+                    
+                    for item in self.questions {
+                        for ans in item.correctAnswer{
+                            if ans.prefix(1) == "T" && item.usersWhoAnswered[item.correctAnswer.firstIndex(of: ans)!] == Auth.auth().currentUser?.email{
+                                self.myQuestionsAnswered += 1
+                            }
+                        }
+                    }
+                    self.points.text = "Points: \(String(self.myQuestionsAnswered))"
+                }
+            }
+        }
+    }
 
     @IBAction func logoutPressed(_ sender: UIButton) {
         let firebaseAuth = Auth.auth()
