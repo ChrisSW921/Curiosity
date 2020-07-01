5624f895ab5d4d74c93715e3107b6e2bae23de32
Chris <withscottc921@gmail.com>
2020-07-01 09:15:52 -0600

    Added my question functionality to choose which questions are correct-only updates db currently

--- Curiosity/Controllers/AnswersTableViewController.swift
+++ Curiosity/Controllers/AnswersTableViewController.swift
@@ -1,80 +1,66 @@
-////
-////  AnswersTableViewController.swift
-////  Curiosity
-////
-////  Created by Chris Withers on 6/25/20.
-////  Copyright © 2020 Chris Withers. All rights reserved.
-////
 //
-//import UIKit
-//import Firebase
+//  AnswersTableViewController.swift
+//  Curiosity
 //
-//class AnswersTableViewController: UITableViewController {
+//  Created by Chris Withers on 6/25/20.
+//  Copyright © 2020 Chris Withers. All rights reserved.
 //
-//    var currentQuestion = ""
-//
-//    var Answers: [Answer] = []
-//
-//    let db = Firestore.firestore()
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        var questionToAnswer = Answer()
-//        questionToAnswer.answer = "Q: \(currentQuestion)"
-//        var divider = Answer()
-//        divider.answer = "--------------------------------"
-//        Answers.append(questionToAnswer)
-//        Answers.append(divider)
-//        loadQuestions()
-//
-//    }
-//
-//
-//    func loadQuestions() {
-//    db.collection("Answers")
-//            .addSnapshotListener { (querySnapshot, error) in
-//            if let e = error {
-//                print("There was an issue retrieving data from firestore. \(e)")
-//            }else {
-//                if let snapshotDocuments = querySnapshot?.documents {
-//                    for doc in snapshotDocuments {
-//                        let data = doc.data()
-//                        if let answerText = data["Answer"] as? String, let question = data["Question"] as? String, let user = data["User"] as? String, let correctAnswer = data["Correct Answer"] as? Bool {
-//                            var newAnswer = Answer()
-//                            newAnswer.question = question
-//                            newAnswer.correctAnswer = correctAnswer
-//                            newAnswer.answer = "A: \(answerText)"
-//                            newAnswer.user = user
-//                            if question == self.currentQuestion {
-//                                self.Answers.append(newAnswer)
-//                            }
-//                            DispatchQueue.main.async {
-//                                self.tableView.reloadData()
-//                            }
-//
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//    }
-//
-//    // MARK: - Table view data source
-//
-//
-//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        // #warning Incomplete implementation, return the number of rows
-//        return Answers.count
-//    }
-//
-//
-//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "MyQuestionsAnswers", for: indexPath)
-//
-//        cell.textLabel!.numberOfLines = 0
-//        cell.textLabel!.text = Answers[indexPath.row].answer
-//
-//        return cell
-//    }
-//
-//}
+
+import UIKit
+import Firebase
+
+class AnswersTableViewController: UITableViewController {
+
+    var currentQuestion = Question()
+
+    let db = Firestore.firestore()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+
+    // MARK: - Table view data source
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return currentQuestion.answers.count
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        var newAnswers: [String] = []
+        for x in currentQuestion.correctAnswer{
+            if currentQuestion.correctAnswer.firstIndex(of: x) == indexPath.row {
+                if x.prefix(1) == "F"{
+                    print("first one activated")
+                    print(x)
+                    newAnswers.append("T \(currentQuestion.usersWhoAnswered[indexPath.row])")
+                }else {
+                    print("Second one activated")
+                    print(x)
+                    newAnswers.append("F \(currentQuestion.usersWhoAnswered[indexPath.row])")
+                }
+            }else {
+                print("None activated")
+                print(x)
+                newAnswers.append(x)
+            }
+        }
+        currentQuestion.correctAnswer = newAnswers
+        db.collection("Questions").document(currentQuestion.question).updateData(["CorrectAnswer": newAnswers])
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyQuestionsAnswers", for: indexPath)
+
+        cell.textLabel!.numberOfLines = 0
+        cell.textLabel!.text = currentQuestion.answers[indexPath.row]
+
+        return cell
+    }
+
+}
--- Curiosity/Controllers/MyQuestionsTableViewController.swift
+++ Curiosity/Controllers/MyQuestionsTableViewController.swift
@@ -1,82 +1,86 @@
-////
-////  MyQuestionsTableViewController.swift
-////  Curiosity
-////
-////  Created by Chris Withers on 6/24/20.
-////  Copyright © 2020 Chris Withers. All rights reserved.
-////
 //
-//import UIKit
-//import Firebase
+//  MyQuestionsTableViewController.swift
+//  Curiosity
 //
-//class MyQuestionsTableViewController: UITableViewController {
+//  Created by Chris Withers on 6/24/20.
+//  Copyright © 2020 Chris Withers. All rights reserved.
 //
-//    var questions: [Question] = []
-//
-//    let db = Firestore.firestore()
-//
-//    var currentSelectedQuestion = ""
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        loadQuestions()
-//
-//    }
-//
-//    func loadQuestions() {
-//    db.collection("Questions")
-//            .addSnapshotListener { (querySnapshot, error) in
-//            self.questions = []
-//            if let e = error {
-//                print("There was an issue retrieving data from firestore. \(e)")
-//            }else {
-//                if let snapshotDocuments = querySnapshot?.documents {
-//                    for doc in snapshotDocuments {
-//                        let data = doc.data()
-//                        if let questionText = data["Question"] as? String, let categoryOfQ = data["Category"] as? String, let user = data["User"] as? String {
-//                            var newQuestion = Question()
-//                            newQuestion.category = categoryOfQ
-//                            newQuestion.question = questionText
-//                            newQuestion.user = user
-//                            if user == Auth.auth().currentUser?.email {
-//                                self.questions.append(newQuestion)
-//                            }
-//                            DispatchQueue.main.async {
-//                                self.tableView.reloadData()
-//                            }
-//
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//    }
-//
-//    // MARK: - Table view data source
-//
-//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        // #warning Incomplete implementation, return the number of rows
-//        return questions.count
-//    }
-//
-//
-//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "MyQuestions", for: indexPath)
-//
-//        cell.textLabel!.numberOfLines = 0
-//        cell.textLabel!.text = questions[indexPath.row].question
-//
-//        return cell
-//    }
-//
-//    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        currentSelectedQuestion = questions[indexPath.row].question
-//        performSegue(withIdentifier: "goToMyAnswers", sender: self)
-//    }
-//
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        let destinationVC = segue.destination as! AnswersTableViewController
-//        destinationVC.currentQuestion = currentSelectedQuestion
-//    }
-//
-//}
+
+import UIKit
+import Firebase
+
+class MyQuestionsTableViewController: UITableViewController {
+
+    var questions: [Question] = []
+
+    let db = Firestore.firestore()
+
+    var currentSelectedQuestion = Question()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadQuestions()
+
+    }
+
+    func loadQuestions() {
+    db.collection("Questions")
+            .addSnapshotListener { (querySnapshot, error) in
+            self.questions = []
+            if let e = error {
+                print("There was an issue retrieving data from firestore. \(e)")
+            }else {
+                if let snapshotDocuments = querySnapshot?.documents {
+                    for doc in snapshotDocuments {
+                        let data = doc.data()
+                        if let questionText = data["Question"] as? String, let categoryOfQ = data["Category"] as? String, let user = data["User"] as? String, let QsAnswers = data["Answers"] as? [String], let UsersWhoAnswered = data["usersWhoAnswered"] as? [String], let correctA = data["CorrectAnswer"] as? [String] {
+                            var newQuestion = Question()
+                            newQuestion.category = categoryOfQ
+                            newQuestion.question = questionText
+                            newQuestion.user = user
+                            newQuestion.answers = QsAnswers
+                            newQuestion.correctAnswer = correctA
+                            newQuestion.usersWhoAnswered = UsersWhoAnswered
+                            if newQuestion.user == Auth.auth().currentUser?.email {
+                                self.questions.append(newQuestion)
+                            }
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                            }
+                            
+                        }
+                    
+                    }
+                }
+            }
+        }
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return questions.count
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyQuestions", for: indexPath)
+
+        cell.textLabel!.numberOfLines = 0
+        cell.textLabel!.text = questions[indexPath.row].question
+
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        currentSelectedQuestion = questions[indexPath.row]
+        performSegue(withIdentifier: "goToMyAnswers", sender: self)
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! AnswersTableViewController
+        destinationVC.currentQuestion = currentSelectedQuestion
+    }
+
+}
